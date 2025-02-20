598f2212d7a8031fb79629313df2f8447f9183c0
sh-sofi <shkoropadsofij@gmail.com>
2025-02-21 00:23:19 +0200

    Add Drivers component

--- src/app/components/drivers/drivers.component.ts
+++ src/app/components/drivers/drivers.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TransportService } from '../../services/transport.service';
+import { Driver } from '../../models/driver.model';
 @Component({
   selector: 'app-drivers',
   standalone: false,
   templateUrl: './drivers.component.html',
-  styleUrl: './drivers.component.scss'
+  styleUrl: './drivers.component.scss',
 })
-export class DriversComponent {
+export class DriversComponent implements OnInit {
+  drivers: Driver[] = [];
 
+  constructor(private transportService: TransportService) {}
+
+  ngOnInit(): void {
+    this.transportService.getDrivers().subscribe((drivers) => {
+      this.drivers = drivers;
+    });
+  }
 }
