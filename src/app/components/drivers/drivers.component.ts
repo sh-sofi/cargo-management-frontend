78ccf77665a6c41891f2a847bcd12edba5b729d1
sh-sofi <shkoropadsofij@gmail.com>
2025-02-23 19:57:31 +0200

    Change Drivers page styles

--- src/app/components/drivers/drivers.component.ts
+++ src/app/components/drivers/drivers.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TransportService } from '../../services/transport.service';
 import { Driver } from '../../models/driver.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
 @Component({
   selector: 'app-drivers',
   standalone: false,
@@ -8,13 +12,24 @@ import { Driver } from '../../models/driver.model';
   styleUrl: './drivers.component.scss',
 })
 export class DriversComponent implements OnInit {
-  drivers: Driver[] = [];
+  displayedColumns: string[] = [
+    'lastName',
+    'firstName',
+    'middleName',
+    'experience',
+  ];
+  dataSource = new MatTableDataSource<Driver>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private transportService: TransportService) {}
 
   ngOnInit(): void {
     this.transportService.getDrivers().subscribe((drivers) => {
-      this.drivers = drivers;
+      this.dataSource.data = drivers;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 }
